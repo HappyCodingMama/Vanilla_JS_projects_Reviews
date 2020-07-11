0cb078255ecc4c5e07c4bf68acabcabc63dc5166
HappyCodingMama <happycodingmama@gmail.com>
2020-07-11 23:25:50 +0900

    complete the project

---
+++ app.js
@@ -0,0 +1,87 @@
+//local reviews data
+const reviews = [
+    {
+        id: 1,
+        name: "susan smith",
+        job: "web developer",
+        img: "./img/person-1.png",
+        text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+    },
+    {
+        id: 2,
+        name: "Adam jordan",
+        job: "frontend developer",
+        img: "./img/person-2.png",
+        text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+    },
+    {
+        id: 3,
+        name: "Last Christmas",
+        job: "motion designer",
+        img: "./img/person-3.png",
+        text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+    },
+    {
+        id: 4,
+        name: "New Year",
+        job: "lawyer",
+        img: "./img/person-4.png",
+        text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+    },
+
+];
+
+//select items
+const img = document.getElementById("person-img");
+const author = document.getElementById("author");
+const job = document.getElementById("job");
+const info = document.getElementById("info");
+
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+const randomBtn = document.querySelector(".random-btn");
+
+// set starting item
+let currentItem = 0;
+
+// load initial item
+window.addEventListener("DOMContentLoaded", function () {
+   showPerson();
+});
+
+// show person based on item
+
+function showPerson() {
+    const item = reviews[currentItem];
+    img.src = item.img;
+    author.textContent = item.name;
+    job.textContent = item.job;
+    info.textContent = item.text;
+};
+
+//show next person
+
+nextBtn.addEventListener("click", function() {
+    currentItem++;
+    if(currentItem > reviews.length - 1) {
+        currentItem = 0;
+    }
+    showPerson();
+});
+
+
+//show prev person
+
+prevBtn.addEventListener("click", function() {
+    currentItem--;
+    if(currentItem <  0) {
+        currentItem = reviews.length -1;
+    }
+    showPerson();
+});
+
+// show random person
+randomBtn.addEventListener("click", function() {
+    currentItem = Math.floor(Math.random() * reviews.length);
+    showPerson();
+});
